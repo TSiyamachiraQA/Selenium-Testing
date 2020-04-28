a5b3314dcb729353a8d05c3519cc2edf1800bf51
Tsiyamachiraqa <qasiyamachira@gmail.com>
2020-04-28 15:31:37 +0100

    added the before and after methods

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,6 +1,28 @@
 package org.example;
 
 
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AppTest {
+
+
+    private WebDriver driver;
+
+
+    @Before
+    public void setUp(){
+        driver = new ChromeDriver();
+
+    }
+
+
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
 
 }
